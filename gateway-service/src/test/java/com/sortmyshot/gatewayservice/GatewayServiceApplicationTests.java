fafb950dd6916557a6c3fff36cc3fdd9395d44c2
Spencer Stewart <spencerjamesstewart@gmail.com>
2023-10-31 18:58:40 -0500

    Init gateway service.

---
+++ gateway-service/src/test/java/com/sortmyshot/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sortmyshot.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
