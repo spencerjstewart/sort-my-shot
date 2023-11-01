b644df711417e19298f341029226ba3bb1dcf86f
Spencer Stewart <spencerjamesstewart@gmail.com>
2023-10-31 20:13:06 -0500

    Init frontend service.

---
+++ frontend-service/src/test/java/com/sortmyshot/frontendservice/FrontendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sortmyshot.frontendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
