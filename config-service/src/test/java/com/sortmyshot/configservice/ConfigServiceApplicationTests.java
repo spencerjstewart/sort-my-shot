ac91fe464c5d6526fadf74bed38b598d732d4d53
Spencer Stewart <spencerjamesstewart@gmail.com>
2023-10-31 19:59:49 -0500

    Init config service.

---
+++ config-service/src/test/java/com/sortmyshot/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sortmyshot.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
