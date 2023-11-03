57e945a9ff3c07baa1a4e02bb5e0f1946258acbe
Spencer Stewart <spencerjamesstewart@gmail.com>
2023-11-03 12:41:44 -0500

    Make http authorization on path specific.

--- user-service/src/main/java/com/sortmyshot/userservice/config/WebSecurity.java
+++ user-service/src/main/java/com/sortmyshot/userservice/config/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,10 +15,9 @@ public class WebSecurity {
     public SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
-            cust -> {
-                cust.anyRequest().permitAll();
-            }
-        );
+                cust -> {
+                    cust.requestMatchers(new AntPathRequestMatcher("/test")).permitAll();
+                });
 
         return http.build();
     }
